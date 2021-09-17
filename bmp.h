ae3cb7aca17a1c544772e30b0c6566b89f505a05
stefanpatrichi <stefanpatrichi07@gmail.com>
2021-09-17 20:21:43 +0300

    initial commit

---
+++ bmp.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <cstdint>
+#include <fstream>
+#include <vector>
+
+/**
+ * common data types
+ * 
+ * https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-dtyp/efda8314-6e41-4837-8299-38ba0ee04b92
+ */
+using BYTE  = uint8_t;
+using DWORD = uint32_t;
+using LONG  = int32_t;
+using WORD  = uint16_t;
+
+/**
+ * a BITMAPFILEHEADER contains info about the type, size, layout of a BMP file
+ *
+ * https://docs.microsoft.com/en-gb/windows/win32/api/wingdi/ns-wingdi-bitmapfileheader
+ */
+struct BITMAPFILEHEADER {
+  WORD  bfType;
+  DWORD bfSize;
+  WORD  bfReserved1;
+  WORD  bfReserved2;
+  DWORD bfOffBits;
+} __attribute__((__packed__));
+
+/**
+ * a BITMAPINFOHEADER contains info about the dimensions and colour size of a BMP file
+ *
+ * https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-bitmapinfoheader
+ */
+struct BITMAPINFOHEADER {
+  DWORD biSize;
+  LONG  biWidth;
+  LONG  biHeight;
+  WORD  biPlanes;
+  WORD  biBitCount;
+  DWORD biCompression;
+  DWORD biSizeImage;
+  LONG  biXPelsPerMeter;
+  LONG  biYPelsPerMeter;
+  DWORD biClrUsed;
+  DWORD biClrImportant;
+} __attribute__((__packed__));
+
+/**
+ * RGBTRIPLE describes a colour consisting of relative intensities of red, green and blue.
+ *
+ * https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-rgbtriple
+ */
+struct RGBTRIPLE {
+  WORD rgbtBlue;
+  WORD rgbtGreen;
+  WORD rgbtRed;
+
+  RGBTRIPLE(int blue, int green, int red);
+  RGBTRIPLE(const RGBTRIPLE&);
+  RGBTRIPLE& operator=(RGBTRIPLE);
+
+  RGBTRIPLE& operator+=(const RGBTRIPLE&);
+  RGBTRIPLE& operator-=(const RGBTRIPLE&);
+  RGBTRIPLE operator+(const RGBTRIPLE&);
+  RGBTRIPLE operator-(const RGBTRIPLE&);
+} __attribute__((__packed__));
+
+
+
+class BMPImage {
+public:
+  BMPImage(const char* filename);
+  BMPImage(int height, int width);
+
+  BMPImage(BMPImage&&)            = delete;
+  BMPImage(const BMPImage&)       = delete;
+  void operator=(BMPImage&&)      = delete;
+  void operator=(const BMPImage&) = delete;
+
+  void read(const char* filename);
+  void write(const char* filename);
+  void blur();
+private:
+  BITMAPFILEHEADER file_header;
+  BITMAPINFOHEADER info_header;
+  std::vector<std::vector<WORD>> data;
+
+  void readHeaders(std::ifstream&);
+  void writeHeaders(std::ofstream&);
+};
---
+++ filter.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+
+#include "bmp.h"
+
+static const int DEFAULT_RADIUS = 3;
+
+int main(int argc, const char* argv[]) {
+  std::ios::sync_with_stdio(0);
+  int radius = DEFAULT_RADIUS;
+
+  if (argc != 2 && argc != 3) 
+    throw std::runtime_error( 
+      "Folosire: ./filter imagine_intrare.bmp imagine_iesire.bmp [raza] (*nix)\n"
+      "          filter.exe imagine_intrare.bmp imagine_iesire.bmp [raza] (Windows)\n"
+    );
+  
+  if (argc == 3) {
+    try {
+      radius = std::stoi(argv[argc - 1]);
+    } catch (std::invalid_argument& e) {
+      std::cerr << "Raza invalida\n";
+    } catch (std::out_of_range& e) {
+      std::cerr << "Raza in afara limitelor admise\n";
+    }
+    --argc;
+  }
+
+  // read image
+  BMPImage image(argv[argc - 2]);
+
+  image.blur();
+  image.write(argv[argc - 1]);
+
+  return 0;
+}
