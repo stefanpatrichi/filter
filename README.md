f45d862e5d23d3aff2e44551c141f4d19b404e23
stefanpatrichi <stefanpatrichi07@gmail.com>
2021-10-01 16:39:47 +0300

    change to c version i made for cs50

--- README.md
+++
@@ -1,2 +0,0 @@
-## filter
-Personal programming project involving applying various filters to 24-bit BMP images. Inspired by CS50's 2020 pset4.
--- build/CMakeFiles/filter.dir/cmake_clean.cmake
+++ build/CMakeFiles/filter.dir/cmake_clean.cmake
@@ -1,13 +1,13 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/filter.dir/src/bmp.cpp.o"
-  "CMakeFiles/filter.dir/src/bmp.cpp.o.d"
-  "CMakeFiles/filter.dir/src/filter.cpp.o"
-  "CMakeFiles/filter.dir/src/filter.cpp.o.d"
+  "CMakeFiles/filter.dir/src/bmp.c.o"
+  "CMakeFiles/filter.dir/src/bmp.c.o.d"
+  "CMakeFiles/filter.dir/src/filter.c.o"
+  "CMakeFiles/filter.dir/src/filter.c.o.d"
   "filter"
   "filter.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C)
   include(CMakeFiles/filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
